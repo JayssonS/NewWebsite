00d71ff2914698d9b92bc65f2c8f8737566780d9
Jeroxoid <Jeroxoid@outlook.com>
2023-08-31 01:50:09 -0400

    LOL
    I was working in the wrong file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           window.scrollTo(0,0);
         }}
         >
-          <img src="/src/assets/logo-no-background.png" alt = "logo" className = "w-9 h-9 object-contain"/>
+          <img src="src/assets/logo-no-background.png" alt = "logo" className = "w-9 h-9 object-contain"/>
           <p className ="text-white text-[18px] font-bold cursor-pointer flex">Jaysson Solano&nbsp;<span className = "sm:block hidden">| JayCilantro</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
